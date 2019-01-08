7bdef85a68a0b4c7c00644ee30af959e5863d67c
Ando <andranik@nvision.am>
2019-01-08 17:44:23 +0400

    added new experiments

--- test_model.py
+++ test_model.py
@@ -11,6 +11,7 @@ from noise_layers.noiser import Noiser
 from PIL import Image
 import torchvision.transforms.functional as TF
 
+
 def randomCrop(img, height, width):
     assert img.shape[0] >= height
     assert img.shape[1] >= width
@@ -33,8 +34,8 @@ def main():
     parser.add_argument('--batch-size', '-b', default=12, type=int, help='The batch size.')
     parser.add_argument('--source-image', '-s', required=True, type=str,
                         help='The image to watermark')
-    parser.add_argument('--times', '-t', default=10, type=int,
-                        help='Number iterations (insert watermark->extract).')
+    # parser.add_argument('--times', '-t', default=10, type=int,
+    #                     help='Number iterations (insert watermark->extract).')
 
     args = parser.parse_args()
 
@@ -45,7 +46,6 @@ def main():
     hidden_net = Hidden(hidden_config, device, noiser, None)
     utils.model_from_checkpoint(hidden_net, checkpoint)
 
-    np.random.seed(687687)
 
     image_pil = Image.open(args.source_image)
     image = randomCrop(np.array(image_pil), hidden_config.H, hidden_config.W)
@@ -53,16 +53,18 @@ def main():
     image_tensor = image_tensor * 2 - 1  # transform from [0, 1] to [-1, 1]
     image_tensor.unsqueeze_(0)
 
-    for t in range(args.times):
-        message = torch.Tensor(np.random.choice([0, 1], (image_tensor.shape[0],
-                                                                   hidden_config.message_length))).to(device)
-        losses, (encoded_images, noised_images, decoded_messages) = hidden_net.validate_on_batch([image_tensor, message])
-        decoded_rounded = decoded_messages.detach().cpu().numpy().round().clip(0, 1)
-        message_detached = message.detach().cpu().numpy()
-        print('original: {}'.format(message_detached))
-        print('decoded : {}'.format(decoded_rounded))
-        print('error : {:.3f}'.format(np.mean(np.abs(decoded_rounded - message_detached))))
-        # bitwise_avg_err = np.sum(np.abs(decoded_rounded - message.detach().cpu().numpy()))/(image_tensor.shape[0] * messages.shape[1])
+    # for t in range(args.times):
+    message = torch.Tensor(np.random.choice([0, 1], (image_tensor.shape[0],
+                                                    hidden_config.message_length))).to(device)
+    losses, (encoded_images, noised_images, decoded_messages) = hidden_net.validate_on_batch([image_tensor, message])
+    decoded_rounded = decoded_messages.detach().cpu().numpy().round().clip(0, 1)
+    message_detached = message.detach().cpu().numpy()
+    print('original: {}'.format(message_detached))
+    print('decoded : {}'.format(decoded_rounded))
+    print('error : {:.3f}'.format(np.mean(np.abs(decoded_rounded - message_detached))))
+    utils.save_images(image_tensor.cpu(), encoded_images.cpu(), 'test', '.', resize_to=(256, 256))
+
+    # bitwise_avg_err = np.sum(np.abs(decoded_rounded - message.detach().cpu().numpy()))/(image_tensor.shape[0] * messages.shape[1])
 
 
 
