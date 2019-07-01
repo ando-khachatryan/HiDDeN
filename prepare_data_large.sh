0e38390aa32f38b6922c40b602709c9fb4d42893
ando <ando.khachatryan@gmail.com>
2019-07-01 15:32:40 +0400

    added bash files to download/create datasets

---
+++ prepare_data_large.sh
@@ -0,0 +1,25 @@
+mkdir -p data
+cd ./data/
+echo "Downloading train2017.zip  dataset from coco..."
+wget http://images.cocodataset.org/zips/train2017.zip
+echo "Download finished"
+
+echo "Unzipping files into train2017/ ..."
+unzip -q train2017.zip
+echo "Done"
+
+echo "Creating large  dataset..."
+mkdir -p large/train/train_class
+mkdir -p large/test/test_class
+
+find ./train2017 -maxdepth 1 -type f -print0 | head -z -n 103000 | xargs -0 -r -- cp -t "large/train/train_class" --
+find ./large/train/train_class -maxdepth 1 -type f -print0 | head -z -n -3000 | xargs -0 -r -- mv -t "large/test/test_class"
+echo "Done"
+echo "./large/train/ $(ls -l ./large/train/train_class | egrep -c '^-') files"
+echo "./large/test/  $(ls -l ./large/test/test_class   | egrep -c '^-') files"
+
+
+echo "Removing train2017 folder..." 
+rm -rf ./train2017
+echo "Done"
+
