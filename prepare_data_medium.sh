878d1a15ede35a0df820302f3e27fdc60a1fd1a6
ando <ando.khachatryan@gmail.com>
2019-07-01 22:41:40 +0400

    bugfix+skip download if exists

--- prepare_data_medium.sh
+++ prepare_data_medium.sh
@@ -1,7 +1,11 @@
 mkdir -p data
 cd ./data/
-echo "Downloading test2017.zip  dataset from coco..."
-wget http://images.cocodataset.org/zips/test2017.zip
+if [! -f ./test2017.zip ]; then
+  echo "Downloading test2017.zip  dataset from coco..."
+  wget http://images.cocodataset.org/zips/test2017.zip
+else
+  echo "test2017.zip already exists, skipping download"
+fi
 echo "Download finished"
 
 echo "Unzipping files into test2017/ ..."
@@ -24,7 +28,7 @@ mkdir -p medium/train/train_class
 mkdir -p medium/val/val_class
 
 find ./test2017 -maxdepth 1 -type f -print0 | head -z -n 11000 | xargs -0 -r -- cp -t "medium/train/train_class" --
-find ./medium/train/train_class -maxdepth 1 -type f -print0 | head -z -n 10000 | xargs -0 -r -- mv -t "medium/val/val_class"
+find ./medium/train/train_class -maxdepth 1 -type f -print0 | head -z -n 1000 | xargs -0 -r -- mv -t "medium/val/val_class"
 echo "Done"
 echo "./medium/train/ $(ls -l ./medium/train/train_class | egrep -c '^-') files"
 echo "./medium/val/  $(ls -l ./medium/val/val_class   | egrep -c '^-') files"
